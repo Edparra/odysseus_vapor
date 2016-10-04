34b553939acdcf5a2f8ad545b63b947c9c0ebc44
Irvin Gonzalez <glez.irv@gmail.com>
2016-10-04 13:36:21 -0400

    scaffolded wishlist and added new dbs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003180717) do
+ActiveRecord::Schema.define(version: 20161004165025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20161003180717) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "products_wishlists", id: false, force: :cascade do |t|
+    t.integer "product_id",  null: false
+    t.integer "wishlist_id", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
@@ -31,4 +36,12 @@ ActiveRecord::Schema.define(version: 20161003180717) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "wishlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wishlists_on_user_id", using: :btree
+  end
+
+  add_foreign_key "wishlists", "users"
 end
