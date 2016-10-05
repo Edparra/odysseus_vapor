1ae7504a21e625dc982c933e89813301f177fdd4
Edparra <Edprr4@gmail.com>
2016-10-05 00:53:01 -0400

    added devise and more nav edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, :phone, :email, presence: true
+  validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   resources :wishlists
   resources :products
+
+ get 'user_root', to: 'wishlists#index', as: :user_root
+
   get 'users/index'
-
   get 'users/show'
-
   get 'users/new'
-
   get 'users/edit'
+
+
 
   get 'welcome/homepage'
   get 'welcome/about'
   get 'welcome/contact'
   root 'welcome#homepage'
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
