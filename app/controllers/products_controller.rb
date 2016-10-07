c48e3bbd0ac9a4789d61f768f3fdf9cb73344487
Irvin Gonzalez <glez.irv@gmail.com>
2016-10-07 14:52:35 -0400

    fixed add to wishlist btn for non-users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:toggle_add]
   before_action :set_product, only: [:show, :edit, :update, :destroy, :toggle_add]
 
   # GET /products
